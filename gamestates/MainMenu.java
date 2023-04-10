e4500930d4c0bb93a4dbec7b8ef198d1e56f58e5
Pob <jeduardoa2003@gmail.com>
2023-04-10 01:21:13 -0400

    MenuStuff

--- gamestates/MainMenu.java
+++ gamestates/MainMenu.java
@@ -3,19 +3,25 @@ package gamestates;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
 
 import maines.*;
+import utilz.LoadImg;
+
 import static utilz.Constants.MainMenuButton.*;
 
 public class MainMenu implements ActionListener{
+	private BufferedImage background;
 	private MyPanel Mypanel;
 	private int Index = 0;
 	public JButton playButton, quitButton;
 	private boolean playButtonOver, quitButtonOver;
+	private Font font = new Font("Mv Boli", Font.BOLD, 45);
 
 	public MainMenu(MyPanel panel){
 		this.Mypanel = panel;
 		initButtons(this.Mypanel);
+		this.background = LoadImg.GetImage(LoadImg.MainMenu);
 	}
 
 	public void update(){
@@ -24,32 +30,29 @@ public class MainMenu implements ActionListener{
 	}
 
 	public void draw(Graphics g){
-		drawButtons(g);
-	}
-
-	private void drawButtons(Graphics g){
-		g.setColor(Color.CYAN);
-		g.fillRect(0, 0, game.Game_Width, game.Game_Height);
+		g.drawImage(background, 0, 0, game.Game_Width, game.Game_Height, null);
 	}
 
 	private void initButtons(MyPanel panel){
+		panel.setLayout(null);
 		this.playButton = new JButton("Play");
-		this.playButton.setFont(new Font("Comic Sans", Font.BOLD, 45));
-		this.playButton.setForeground(Color.WHITE);;
+		this.playButton.setForeground(Color.WHITE);
+		this.playButton.setFont(this.font);
 		this.playButton.setBounds((game.Game_Width/2) - ButtonWidth/2, (game.Game_Height/2) - (ButtonHeight*2), ButtonWidth, ButtonHeight);
 		this.playButton.addActionListener(this);
-		this.playButton.setBackground(Color.LIGHT_GRAY);
 		this.playButton.addMouseListener(panel.mouse);
 		this.playButton.addMouseMotionListener(panel.mouse);
+		this.playButton.setLocation((int)((game.Game_Width * 0.5f) - (ButtonWidth/2)),(int)(game.Game_Height *0.4f));
 		this.Mypanel.add(this.playButton);
+
 		this.quitButton = new JButton("Quit");
-		this.quitButton.setFont(new Font("Comic Sans", Font.BOLD, 45));
-		this.quitButton.setForeground(Color.WHITE);;
+		this.quitButton.setForeground(Color.WHITE);
+		this.quitButton.setFont(this.font);
 		this.quitButton.setBounds((game.Game_Width/2) - ButtonWidth/2, (game.Game_Height/2) - (ButtonHeight), ButtonWidth, ButtonHeight);
 		this.quitButton.addActionListener(this);
-		this.quitButton.setBackground(Color.LIGHT_GRAY);
 		this.quitButton.addMouseListener(panel.mouse);
 		this.quitButton.addMouseMotionListener(panel.mouse);
+		this.quitButton.setLocation((int)((game.Game_Width * 0.5f) - (ButtonWidth/2)),(int)(game.Game_Height *0.6f));
 		this.Mypanel.add(quitButton);
 	}
 
@@ -68,8 +71,8 @@ public class MainMenu implements ActionListener{
 	private void selectButton(){
 		playButton.setSelected(false);
 		quitButton.setSelected(false);
-		playButton.setBackground(Color.LIGHT_GRAY);
-		quitButton.setBackground(Color.LIGHT_GRAY);
+		playButton.setBackground(new Color(152, 196, 0));
+		quitButton.setBackground(new Color(196, 0, 54));
 		if(playButtonOver){this.Index = 1;}
 		else if(quitButtonOver){this.Index = 2;}
 		switch(this.Index){
@@ -97,10 +100,10 @@ public class MainMenu implements ActionListener{
 	}
 
 	private void abilitateButtons(){		
-		playButton.setEnabled(true);		
-		quitButton.setEnabled(true);
-		playButton.setVisible(true);
-		quitButton.setVisible(true);
+		this.playButton.setEnabled(true);		
+		this.quitButton.setEnabled(true);
+		this.playButton.setVisible(true);
+		this.quitButton.setVisible(true);
 	}	
 
 	private void inhabiltateButtons(){		
